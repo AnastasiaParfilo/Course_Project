b4e56e1764301bf686c46a531a11e72fe874fcd3
Anastasia Parfilo <parfilo2003@gmail.com>
2022-02-20 12:35:53 +0200

    test

---
+++ src/main/java/ua/com/khai/Building.java
@@ -0,0 +1,101 @@
+package ua.com.khai;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class Building {
+
+    private Integer numberOfRoom;
+    private Integer length;
+    private Integer width;
+
+    private List<String> listOfPeople=new ArrayList<>();
+    private String name;
+    private String lastName;
+    private Integer numberOfPeople;
+
+    public Integer getNumberOfRoom() {
+        return numberOfRoom;
+    }
+
+    public void setNumberOfRoom(Integer numberOfRoom) {
+        this.numberOfRoom = numberOfRoom;
+    }
+
+    public Integer getLength() {
+        return length;
+    }
+
+    public void setLength(Integer length) {
+        this.length = length;
+    }
+
+    public Integer getWidth() {
+        return width;
+    }
+
+    public void setWidth(Integer width) {
+        this.width = width;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Integer getNumberOfPeople() {
+        return numberOfPeople;
+    }
+
+    public void setNumberOfPeople(Integer numberOfPeople) {
+        this.numberOfPeople = numberOfPeople;
+    }
+
+    @Override
+    public String toString() {
+        return "Building{" +
+                ", numberOfRoom=" + numberOfRoom +
+                ", length=" + length +
+                ", width=" + width +
+                ", name='" + name + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", numberOfPeople=" + numberOfPeople +
+                '}';
+    }
+
+
+    public Integer areaOfRoom(){
+        Integer area=length*width;
+        return area;
+    }
+
+    public Integer perimOfRoom(){
+        Integer perim=length+length+width+width;
+        return perim;
+    }
+
+    public Integer areaOfBuilding(){
+        Integer area=numberOfRoom*areaOfRoom();
+        return area;
+    }
+
+
+    public void addPeople(){
+        listOfPeople.add(name+" "+lastName);
+    }
+
+    public List<String> getListOfPeoplePeople(){
+        return listOfPeople;
+    }
+}
---
+++ src/main/java/ua/com/khai/Residential.java
@@ -0,0 +1,54 @@
+package ua.com.khai;
+
+public class Residential extends Building{
+    private Integer playground;
+    private Integer malls;
+    private Integer schools;
+    private Integer kindergarten;
+
+    public Integer getPlayground() {
+        return playground;
+    }
+
+    public void setPlayground(Integer playground) {
+        this.playground = playground;
+    }
+
+    public Integer getMalls() {
+        return malls;
+    }
+
+    public void setMalls(Integer malls) {
+        this.malls = malls;
+    }
+
+    public Integer getSchools() {
+        return schools;
+    }
+
+    public void setSchools(Integer schools) {
+        this.schools = schools;
+    }
+
+    public Integer getKindergarten() {
+        return kindergarten;
+    }
+
+    public void setKindergarten(Integer kindergarten) {
+        this.kindergarten = kindergarten;
+    }
+
+    public Integer costOfRent(){
+        return areaOfRoom()*10+playground*3+kindergarten*4+schools*5+malls*2;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString()+"Residential{" +
+                "playground=" + playground +
+                ", malls=" + malls +
+                ", schools=" + schools +
+                ", kindergarten=" + kindergarten +
+                '}';
+    }
+}
