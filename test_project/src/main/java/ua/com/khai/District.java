513444062ee44557cabe6809cd1924ca44e49d05
olenavit <o.s.vitkovska@student.khai.edu>
2022-04-25 19:32:27 +0300

    add District

--- test_project/src/main/java/ua/com/khai/District.java
+++ test_project/src/main/java/ua/com/khai/District.java
@@ -2,12 +2,46 @@ package ua.com.khai;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 import java.util.stream.Collectors;
 
 public class District {
 
     List<Building> districtList = new ArrayList<>();
 
+    public void create(){
+        Scanner sc=new Scanner(System.in);
+        int i = 3;
+        while (i != 0) {
+            System.out.println();
+            System.out.println("Please choose some building to add");
+            System.out.println("If you want to add Office, enter 1");
+            System.out.println("If you want to add Residential, enter 2");
+            System.out.println("If you want to add Warehouse, enter 3");
+            System.out.println("If don't want to add, enter 0");
+            System.out.println();
+            i = sc.nextInt();
+            switch (i) {
+                case 1 -> {
+                    //ТУТ РАНДОМНОЕ ИЛИ ИЗ ФАЙЛА ЗАПОЛНЕНИЕ
+                    System.out.println("Office was added");
+                }
+                case 2 -> {
+                    //ТУТ РАНДОМНОЕ ИЛИ ИЗ ФАЙЛА ЗАПОЛНЕНИЕ
+
+                    System.out.println("Residential was added");
+                }
+                case 3 -> {
+                    //ТУТ РАНДОМНОЕ ИЛИ ИЗ ФАЙЛА ЗАПОЛНЕНИЕ
+                    System.out.println("Warehouse was added");
+                }
+                default -> System.out.println();
+            }
+        }
+    }
+    public void delete(){
+        districtList.clear();
+    }
     public void add(Building building){
         districtList.add(building);
     }
@@ -24,7 +58,7 @@ public class District {
     public List<Building> getWarehouse(){
         return districtList.stream().filter(p -> p.getClass().equals(Warehouse.class)).collect(Collectors.toList());
     }
-    public List<Building> getAll(){
+    public List<Building> show(){
         return districtList;
     }
 }
--- test_project/src/main/java/ua/com/khai/Main.java
+++ test_project/src/main/java/ua/com/khai/Main.java
@@ -2,20 +2,26 @@ package ua.com.khai;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         District district = new District();
-        Office office1 = new Office();
-        office1.setNumArendators(200);
-        office1.setPriceRent(100);
-        Residential residential = new Residential();
-        residential.setNumberOfRoom(200);
-        district.add(office1);
-        district.add(residential);
-        System.out.println(district.getOffice());
-        district.remove(office1);
-        System.out.println(district.getOffice());
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Hello, user!!");
+        while (true) {
+            System.out.println("If you want to create district, enter 1");
+            System.out.println("If you want to delete district, enter 2");
+            System.out.println("If you want to show district, enter 3");
+            System.out.println("If you want to exit, enter 0");
+            int i = sc.nextInt();
+            switch (i){
+                case 1 -> district.create();
+                case 2 -> district.delete();
+                case 3 -> System.out.println(district.show());
+                case 0 -> System.exit(0);
+            }
+        }
 
     }
 }
