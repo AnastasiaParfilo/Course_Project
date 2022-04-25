1cc244bcc3e3543d682f7a27049783cab8447dfe
olenavit <o.s.vitkovska@student.khai.edu>
2022-04-25 19:03:24 +0300

    add District

--- test_project/src/main/java/ua/com/khai/District.java
+++ test_project/src/main/java/ua/com/khai/District.java
@@ -1,4 +1,30 @@
 package ua.com.khai;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class District {
+
+    List<Building> districtList = new ArrayList<>();
+
+    public void add(Building building){
+        districtList.add(building);
+    }
+
+    public void remove(Building building){
+        districtList.remove(building);
+    }
+    public List<Building> getOffice(){
+        return districtList.stream().filter(p -> p.getClass().equals(Office.class)).collect(Collectors.toList());
+    }
+    public List<Building> getResidential(){
+        return districtList.stream().filter(p -> p.getClass().equals(Residential.class)).collect(Collectors.toList());
+    }
+    public List<Building> getWarehouse(){
+        return districtList.stream().filter(p -> p.getClass().equals(Warehouse.class)).collect(Collectors.toList());
+    }
+    public List<Building> getAll(){
+        return districtList;
+    }
 }
--- test_project/src/main/java/ua/com/khai/Main.java
+++ test_project/src/main/java/ua/com/khai/Main.java
@@ -1,4 +1,21 @@
 package ua.com.khai;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
+    public static void main(String[] args) {
+        District district = new District();
+        Office office1 = new Office();
+        office1.setNumArendators(200);
+        office1.setPriceRent(100);
+        Residential residential = new Residential();
+        residential.setNumberOfRoom(200);
+        district.add(office1);
+        district.add(residential);
+        System.out.println(district.getOffice());
+        district.remove(office1);
+        System.out.println(district.getOffice());
+
+    }
 }
